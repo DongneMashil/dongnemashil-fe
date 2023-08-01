d31f1386cf567300f0e134a029709cb45b04c4ad
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-01 21:52:46 +0900

    update: App.tsx RTK 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import Router from './routes/Router';
+import Router from 'routes/Router';
 import GlobalStyle from 'style/GlobalStyle';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from 'queries/queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import store from 'redux/config/configureStore';
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
-        <Router />
+        <Provider store={store}>
+          <Router />
+        </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
