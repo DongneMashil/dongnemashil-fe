250dc19eeb4e395b0bbc0409cb2e909d3141831a
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-01 17:05:01 +0900

    create: Router 초기 세팅

---
+++ src/routes/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
