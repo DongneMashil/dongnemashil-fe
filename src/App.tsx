f953b0d2cd8fa24dbfc007c760feeeb563d39fa5
Taehyun Kim <agart319@gmail.com>
2023-08-02 18:34:02 +0900

    create : Recoil 설치 및 세팅, 예시파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import GlobalStyle from 'style/GlobalStyle';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from 'queries/queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Router />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </RecoilRoot>
     </>
   );
 }
