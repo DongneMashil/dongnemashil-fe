d7ab2b57b346389b4b8ae6874bef143c8ec8c5a5
Taehyun Kim <agart319@gmail.com>
2023-08-02 11:28:38 +0900

    feat : detailPage 기본기능 추가 (테스트용)

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getReviewDetail, ReviewDetail } from 'api/detailApi';
+import { useParams } from 'react-router-dom';
+
 export const DetailPage = ({}) => {
-  return <div>DetailPage</div>;
+  const { reviewId } = useParams<{ reviewId: string }>();
+  if (!reviewId) {
+    throw new Error('Review ID is missing');
+  }
+  const { data, isLoading, isError, error } = useQuery<ReviewDetail, Error>({
+    queryKey: ['reviewDetail', reviewId],
+    queryFn: () => getReviewDetail(reviewId),
+    enabled: !!reviewId,
+  });
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{String(error)}</div>}
+
+      {data && <div>{data.content}</div>}
+    </div>
+  );
 };
