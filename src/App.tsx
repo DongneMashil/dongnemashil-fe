714558d514a634d89c0679ed58b74ff5e0e871d8
Taehyun Kim <agart319@gmail.com>
2023-08-02 17:58:37 +0900

    delete : redux, context관련 파일 삭제, 패키지 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,13 @@ import GlobalStyle from 'style/GlobalStyle';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from 'queries/queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Provider } from 'react-redux';
-import store from 'redux/config/configureStore';
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
-        <Provider store={store}>
-          <Router />
-        </Provider>
+        <Router />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
