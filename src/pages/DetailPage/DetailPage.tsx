128ebeeac7ea56d42759a560ccd5787e0a420e51
Taehyun Kim <agart319@gmail.com>
2023-08-04 14:14:25 +0900

    chore : 오타수정

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,35 +5,7 @@ import { CommonLayout } from 'components/layout';
 export const DetailPage = () => {
   return (
     <CommonLayout>
-      <div>D6767676etailPage</div>
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>Detai78787lPage</div> <div>DetailPage</div>
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div> <div>DetailPage</div>{' '}
-      <div>DetailPage</div> <div>DetailPage</div>
+      <div>detailpage</div>
     </CommonLayout>
   );
 };
