494f4cc4829177358be5ba32b8c5d41eee4c6ca4
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-04 14:49:34 +0900

    create: Button, Input 스타일링 페이지 추가

---
+++ src/components/common/Button/Button.styles.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const TempButton = styled.button`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: blueviolet;
+  color: #fff;
+`;
