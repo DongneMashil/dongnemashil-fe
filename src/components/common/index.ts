56d3696703559f5753df2905d06022ce05a91a24
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-04 16:43:29 +0900

    chore: export 경로 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from './Button/Button';
 export * from './Tag/Tag';
+export * from './Input/Input';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,4 @@ export * from './HomePage/HomePage';
 export * from './LoginPage/LoginPage';
 export * from './DetailPage/DetailPage';
 export * from './RegisterPage/RegisterPage';
-//export * from './KakaoCallbackPage/KakaoCallbackpage';
+export * from './KakaoCallbackPage/KakaoCallbackpage';
