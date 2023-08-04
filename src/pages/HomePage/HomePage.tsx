bd69dfbf40c59d0598aa41352ed5bfa8ba014eab
Sujin Lee <soolovepat@naver.com>
2023-08-04 21:21:13 +0900

    feat : MapWrapper 컴포넌트 추가

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,15 @@
+import MapWrapper from 'components/common/Map/MapWrapper';
 import NavBar from 'components/layout/NavBar/NavBar';
 import React from 'react';
 
 export const HomePage = () => {
   return (
-    <div>
+    <>
       <NavBar btnLeft={'logo'} btnRight={'myPage'}>
         동네마실
       </NavBar>
+      <MapWrapper />
       HomePage
-    </div>
+    </>
   );
 };
