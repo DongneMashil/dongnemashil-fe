30d4d13107cb5233a070dd0371080f3510e8bb29
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-04 14:47:50 +0900

    update: 로그인 페이지 컴포넌트 수정

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,65 +1,35 @@
-import React, { useState } from 'react';
-import { Input } from 'components/common';
-
-interface LoginValue {
-  email: string;
-  nickname: string;
-  password: string;
-  passwordVerify: string;
-}
+import React from 'react';
+import { useNavigate } from 'react-router';
+import { Button } from 'components/common';
+//import { loginKaKao } from 'api/loginApi';
 
 export const LoginPage = () => {
-  const [loginValue, setLoginValue] = useState<LoginValue>({
-    email: '',
-    nickname: '',
-    password: '',
-    passwordVerify: '',
-  });
+  const navigate = useNavigate();
 
-  const handleValues = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.name);
-    const newData: LoginValue = {
-      ...loginValue,
-      [e.target.name]: e.target.value,
-    };
-    console.log(newData);
-    setLoginValue(newData);
+  const onKakaoHandler = () => {
+    console.log('onKakaoHandler');
+    //loginKaKao();
+  };
+  const onLoginHandler = () => {
+    console.log('onLoginHandler');
+  };
+  const onRegisterHandler = () => {
+    navigate('/register');
   };
 
   return (
-    <>
-      <Input
-        type="email"
-        name="email"
-        id="email"
-        value={loginValue.email}
-        onChange={handleValues}
-      />
-      <p>{loginValue.email}</p>
-      <Input
-        type="text"
-        name="nickname"
-        id="nickname"
-        value={loginValue.nickname}
-        onChange={handleValues}
-      />
-      <p>{loginValue.nickname}</p>
-      <Input
-        type="password"
-        name="password"
-        id="password"
-        value={loginValue.password}
-        onChange={handleValues}
-      />
-      <p>{loginValue.password}</p>
-      <Input
-        type="password"
-        name="passwordVerify"
-        id="passwordVerify"
-        value={loginValue.passwordVerify}
-        onChange={handleValues}
-      />
-      <p>{loginValue.passwordVerify}</p>
-    </>
+    <div
+      style={{
+        width: '450px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+      }}
+    >
+      <p>바로 시작 가능!</p>
+      <Button label="카카오로 로그인" onClick={onKakaoHandler} />
+      <Button label="회원 아이디로 로그인" onClick={onLoginHandler} />
+      <Button label="회원가입" onClick={onRegisterHandler} />
+    </div>
   );
 };
