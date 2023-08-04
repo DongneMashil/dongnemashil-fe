20b6a1bf888c652942f1d03191ef9fadfd6877c3
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-04 14:39:09 +0900

    chore: style 작성중/..

---
+++ src/pages/PostMapPage/PostMapPage.styles.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const StLocationContainer = styled.div`
+  width: 15rem;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+`
+export const StLocationBox = styled.div``
