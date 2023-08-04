8c220716aeeb82bdb22726a9ea3285fbec93dd77
Taehyun Kim <agart319@gmail.com>
2023-08-04 16:22:04 +0900

    update : common레이아웃 라우터로 이동

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,18 @@
+import { CommonLayout } from 'components/layout';
 import { DetailPage, HomePage } from 'pages';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/review/:reviewId" element={<DetailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CommonLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/review/:reviewId" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CommonLayout>
   );
 };
 
