7afda1eba5d56c797ff3ef9873ad659303ff91ae
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-04 14:50:15 +0900

    update: pages 경로 export 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export * from './HomePage/HomePage';
 export * from './LoginPage/LoginPage';
 export * from './DetailPage/DetailPage';
+export * from './RegisterPage/RegisterPage';
+//export * from './KakaoCallbackPage/KakaoCallbackpage';
