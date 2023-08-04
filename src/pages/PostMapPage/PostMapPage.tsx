ba9a9586b3567eb7e6346ebac162f78d22a375ae
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-04 11:45:09 +0900

    docs: 기본적인 html 세팅

--- src/pages/PostMapPage/PostMapPage.tsx
+++ src/pages/PostMapPage/PostMapPage.tsx
@@ -1,4 +1,27 @@
+import { Button } from 'components/common';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 export const PostMapPage = () => {
-  return <div>PostMapPage</div>;
+  const navigate = useNavigate();
+  const handleGoHome = (): void => {
+    navigate('/');
+  };
+
+  const handleGoNextPage = (): void => {
+    navigate('/post');
+  };
+
+  return (
+    <>
+      <div>
+        <Button label="메인으로" onclick={handleGoHome} />
+        <span>좋았던 산책 지점을 선택해주세요</span>
+      </div>
+      <div>지도가 들어갈 부분</div>
+      <div>
+        <div>위치 들어갈 div</div>
+        <Button label="다음버튼" onclick={handleGoNextPage} />
+      </div>
+    </>
+  );
 };
