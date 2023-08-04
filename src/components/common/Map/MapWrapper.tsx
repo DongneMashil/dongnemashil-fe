3da7182479b4a299d15a06b5b885762352b69387
Sujin Lee <soolovepat@naver.com>
2023-08-04 19:16:27 +0900

    create : MapWrapper 컴포넌트 생성

---
+++ src/components/common/Map/MapWrapper.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from './Map';
+
+const MapWrapper = () => {
+  return (
+    <div>
+      <Map width="100%" height="100%" />
+    </div>
+  );
+};
+
+export default MapWrapper;
