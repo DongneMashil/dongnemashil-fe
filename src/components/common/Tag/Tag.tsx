4606827f9647f60e22df12a914ef47ff6d34d7da
Taehyun Kim <agart319@gmail.com>
2023-08-04 09:02:48 +0900

    refactor : tag 스타일링 분리

--- src/components/common/Tag/Tag.tsx
+++ src/components/common/Tag/Tag.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import { TagLayout } from './style';
+
 interface Props {
   text: string;
   onClick: () => void;
@@ -8,18 +9,3 @@ interface Props {
 export const Tag = ({ text, onClick }: Props) => {
   return <TagLayout onClick={onClick}>{text}</TagLayout>;
 };
-
-export const TagLayout = styled.div`
-  background-color: #fff;
-  border-radius: 4px;
-  border: 1px solid #e1e4e8;
-  color: #586069;
-  display: inline-block;
-  font-size: 14px;
-  font-weight: 600;
-  line-height: 1;
-  margin-right: 4px;
-  padding: 3px 5px;
-  vertical-align: middle;
-  white-space: nowrap;
-`;
