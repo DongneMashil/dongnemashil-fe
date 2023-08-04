81541cc2f453e11cfa74ddbcc286364c568bef79
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-04 14:39:09 +0900

    chore: style 작성중/..

--- src/pages/PostMapPage/PostMapPage.tsx
+++ src/pages/PostMapPage/PostMapPage.tsx
@@ -1,6 +1,7 @@
 import { Button } from 'components/common';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { StLocationContainer,StLocationBox } from './PostMapPage.styles';
 export const PostMapPage = () => {
   const navigate = useNavigate();
   const handleGoHome = (): void => {
@@ -18,10 +19,10 @@ export const PostMapPage = () => {
         <span>좋았던 산책 지점을 선택해주세요</span>
       </div>
       <div>지도가 들어갈 부분</div>
-      <div>
-        <div>위치 들어갈 div</div>
+      <StLocationContainer>
+        <StLocationBox className='hello'>위치 들어갈 div</StLocationBox>
         <Button label="다음버튼" onclick={handleGoNextPage} />
-      </div>
+      </StLocationContainer>
     </>
   );
 };
