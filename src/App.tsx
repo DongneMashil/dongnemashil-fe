1272d3c159865907f7af49d8f06d21a9e8a45d95
Taehyun Kim <agart319@gmail.com>
2023-08-04 16:22:04 +0900

    update : common레이아웃 라우터로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from 'queries/queryClient';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
-import { CommonLayout } from 'components/layout';
 
 function App() {
   return (
     <>
-      <CommonLayout>
-        <RecoilRoot>
-          <QueryClientProvider client={queryClient}>
-            <GlobalStyle />
-            <Router />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-        </RecoilRoot>
-      </CommonLayout>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Router />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </RecoilRoot>
     </>
   );
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,19 @@
+import { CommonLayout } from 'components/layout';
 import { DetailPage, HomePage, WriteMapPage } from 'pages';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/review/:reviewId" element={<DetailPage />} />
-        <Route path="/writemap" element={<WriteMapPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CommonLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/review/:reviewId" element={<DetailPage />} />
+          <Route path="/writemap" element={<WriteMapPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CommonLayout>
   );
 };
 
