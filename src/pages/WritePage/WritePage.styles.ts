aa5eecd6a0af2dc3d018d07aade21211c0126135
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-05 02:55:51 +0900

    feat: 이미지 불러오기

---
+++ src/pages/WritePage/WritePage.styles.ts
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const StPlusButton = styled.button`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  position: relative;
+`;
+
+export const StHiddenButton = styled.input`
+  display: none;
+`;
--- src/pages/WritePage/WritePage.tsx
+++ src/pages/WritePage/WritePage.tsx
@@ -1,5 +1,6 @@
 import { Input } from 'components/common';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
+import { StHiddenButton, StPlusButton } from './WritePage.styles';
 
 interface FormValues {
   title: string;
@@ -11,12 +12,42 @@ export const WritePage = () => {
     title: '',
     content: '',
   });
+  const [images, setImages] = useState<string[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormValues({
       ...formValues,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files || []);
+    const imageFiles = files.filter(file => file.type.startsWith('image/'));
+  
+    if (imageFiles.length > 5) {
+      alert('You can select up to 5 images.');
+      return;
+    }
+
+    const newImages = imageFiles.map(file => {
+      return new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = function() {
+          resolve(reader.result as string);
+        }
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+      });
+    });
+
+    Promise.all(newImages).then(setImages);
+  };
+
+  const onButtonClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+    fileInputRef.current?.click();
   };
 
   return (
@@ -30,7 +61,19 @@ export const WritePage = () => {
       />
       <div>
         <div>이미지</div>
-        <input type="file" />
+        <div>
+          <StPlusButton onClick={onButtonClick}>+</StPlusButton>
+          <StHiddenButton
+            ref={fileInputRef}
+            type="file"
+            accept="image/*, video/*"
+            multiple
+            onChange={onFileChange}
+          />
+        </div>
+        {images.map((image, index) => (
+          <img src={image} key={index} alt={`Upload Preview ${index}`} />
+        ))}
       </div>
       <textarea />
     </>
