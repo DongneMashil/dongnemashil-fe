b9afbce90c78357c5a0928f3d405fbacad40565c
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-04 14:50:45 +0900

    update: 버튼 컴포넌트 임시 스타일링 적용, props 인터페이스 수정

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { TempButton } from './Button.styles';
 
 interface InputProps {
   label: string;
-  onclick: () => void;
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-export const Button = ({ label, onclick }: InputProps) => {
-  return <button onClick={onclick}>{label}</button>;
+export const Button = ({ label, onClick }: InputProps) => {
+  return <TempButton onClick={onClick}>{label}</TempButton>;
 };
 
 export default Button;
