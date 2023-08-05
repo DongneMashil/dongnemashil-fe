e7db82017491257030b6f081872c250b7df0c11f
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-06 02:38:35 +0900

    update: 로그인 페이지 NavBar 추가

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 import { Button } from 'components/common';
+import NavBar from 'components/layout/NavBar/NavBar';
 import { loginKakao } from 'api/loginApi';
 
 export const LoginPage = () => {
@@ -18,18 +19,33 @@ export const LoginPage = () => {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '10px',
-        padding: '100px 20px 20px 20px',
-      }}
-    >
-      <p>바로 시작 가능!</p>
-      <Button onClick={onKakaoHandler}>카카오로 로그인</Button>
-      <Button onClick={onLoginHandler}>회원 아이디로 로그인</Button>
-      <Button onClick={onRegisterHandler}>회원가입</Button>
+    <div>
+      <NavBar btnLeft={'logo'} btnRight={'myPage'}>
+        동네마실
+      </NavBar>
+
+      <div
+        style={{
+          width: '200px',
+          height: '200px',
+          margin: '0 auto',
+          backgroundColor: '#ccc',
+          marginTop: '50px',
+        }}
+      ></div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px',
+          padding: '100px 20px 20px 20px',
+        }}
+      >
+        <p>바로 시작 가능!</p>
+        <Button onClick={onKakaoHandler}>카카오로 로그인</Button>
+        <Button onClick={onLoginHandler}>회원 아이디로 로그인</Button>
+        <Button onClick={onRegisterHandler}>회원가입</Button>
+      </div>
     </div>
   );
 };
