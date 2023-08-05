e747fa823512c5626b772d50b04f2f2565a0ef2f
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-06 04:20:34 +0900

    feat: 회원가입 유효성 멘트 체크 기능 추가

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,75 +1,149 @@
 import React, { useState } from 'react';
 import { Input, Button } from 'components/common';
 import { register } from 'api/loginApi';
+import { StVerifyMsg } from './Registerpage.styles';
 
-interface LoginValue {
+interface LoginFormProps {
   email: string;
   nickname: string;
   password: string;
   passwordVerify: string;
 }
 
+interface VerifyMsgProps {
+  email: {
+    msg: string;
+    isValid: boolean;
+  };
+  nickname: {
+    msg: string;
+    isValid: boolean;
+  };
+  password: {
+    msg: string;
+    isValid: boolean;
+  };
+  passwordVerify: {
+    msg: string;
+    isValid: boolean;
+  };
+}
+
 export const RegisterPage = () => {
-  const [loginValue, setLoginValue] = useState<LoginValue>({
+  const [LoginFormProps, setLoginFormProps] = useState<LoginFormProps>({
     email: '',
     nickname: '',
     password: '',
     passwordVerify: '',
   });
 
+  const [verifyMsg, setVerifyMsg] = useState<VerifyMsgProps>({
+    email: {
+      msg: '',
+      isValid: false,
+    },
+    nickname: {
+      msg: '',
+      isValid: false,
+    },
+    password: {
+      msg: '',
+      isValid: false,
+    },
+    passwordVerify: {
+      msg: '',
+      isValid: false,
+    },
+  });
+
+  const emailRegex = new RegExp(`[a-z0-9]+@[a-z]+\\.[a-z]{2,3}`);
+
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.name);
-    const newData: LoginValue = {
-      ...loginValue,
+    const newData: LoginFormProps = {
+      ...LoginFormProps,
       [e.target.name]: e.target.value,
     };
+    const newVerifyMsg: VerifyMsgProps = {
+      ...verifyMsg,
+    };
+    console.log('newVerifyMsg', newVerifyMsg);
     console.log(newData);
-    setLoginValue(newData);
+    setLoginFormProps(newData);
+
+    switch (e.target.type) {
+      case 'email':
+        newVerifyMsg.email = emailRegex.test(e.target.value)
+          ? {
+              msg: '조건 충족',
+              isValid: true,
+            }
+          : {
+              msg: '조건 미충족',
+              isValid: false,
+            };
+        break;
+      case 'nickname':
+        break;
+      case 'password':
+        break;
+      case 'passwordVerify':
+        break;
+      default:
+        break;
+    }
+    setVerifyMsg(newVerifyMsg);
   };
+
   const onSubmitHandler = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
-    register(loginValue.email, loginValue.nickname, loginValue.password);
+    register(
+      LoginFormProps.email,
+      LoginFormProps.nickname,
+      LoginFormProps.password
+    );
   };
 
   return (
     <div>
       <h3>동네마실 환영합니다!</h3>
-
       <h4>이메일</h4>
       <Input
         type="email"
         name="email"
         id="email"
-        value={loginValue.email}
+        value={LoginFormProps.email}
         onChange={onChangeHandler}
       />
-      <p>{loginValue.email}</p>
+      <StVerifyMsg $isValid={verifyMsg.email.isValid}>
+        {verifyMsg.email.msg}
+      </StVerifyMsg>
       <h4>닉네임</h4>
       <Input
         type="text"
         name="nickname"
         id="nickname"
-        value={loginValue.nickname}
+        value={LoginFormProps.nickname}
         onChange={onChangeHandler}
       />
-      <p>{loginValue.nickname}</p>
+      <p>{verifyMsg.nickname.msg}</p>
       <h4>비밀번호</h4>
       <Input
         type="password"
         name="password"
         id="password"
-        value={loginValue.password}
+        value={LoginFormProps.password}
         onChange={onChangeHandler}
       />
-      <p>{loginValue.password}</p>
+      <p>{verifyMsg.password.msg}</p>
       <Input
         type="password"
         name="passwordVerify"
         id="passwordVerify"
-        value={loginValue.passwordVerify}
+        value={LoginFormProps.passwordVerify}
         onChange={onChangeHandler}
       />
-      <p>{loginValue.passwordVerify}</p>
+      <p>{verifyMsg.passwordVerify.msg}</p>
       <Button onClick={onSubmitHandler}>회원가입</Button>
     </div>
   );
