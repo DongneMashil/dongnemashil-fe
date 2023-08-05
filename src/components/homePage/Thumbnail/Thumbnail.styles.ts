50c5a50e74132fbb31790e1a32e2336c365f87cd
Sujin Lee <soolovepat@naver.com>
2023-08-05 11:38:46 +0900

    design : 썸네일 스타일 수정

--- src/components/homePage/Thumbnail/Thumbnail.styles.ts
+++ src/components/homePage/Thumbnail/Thumbnail.styles.ts
@@ -4,4 +4,13 @@ export const StThumbnail = styled.div`
   display: flex;
   flex-direction: column;
   background: #ffffff;
+
+  border-radius: 20px;
+  overflow: hidden;
+
+  & div {
+    padding: 0 30px;
+    position: relative;
+    bottom: 80px;
+  }
 `;
