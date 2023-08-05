10643089101227b48267d2579b0ad3c4cc73439c
Sujin Lee <soolovepat@naver.com>
2023-08-05 09:26:42 +0900

    update : 메인페이지에 썸네일 리스트 추가

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TempButton } from 'components/common';
 import { NavBar } from 'components/layout';
+import { ThumbnailWrapper } from 'components/homePage';
 
 export const HomePage = () => {
   return (
@@ -8,7 +9,7 @@ export const HomePage = () => {
       <NavBar btnLeft={'logo'} btnRight={'myPage'}>
         동네마실
       </NavBar>
-      HomePage
+      <ThumbnailWrapper />
       <div style={{ display: 'flex', gap: '15px', marginTop: '30px' }}>
         <TempButton colorType="black">까망 버튼 :)</TempButton>
         <TempButton colorType="blue">파랑 버튼 :)</TempButton>
