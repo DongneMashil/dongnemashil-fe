e2dd39f5cb2bb59a2154ef2b1792c595b609ca05
Sujin Lee <soolovepat@naver.com>
2023-08-05 09:25:02 +0900

    create : 산책로 썸네일 리스트 component 생성

---
+++ src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Thumbnail } from '../Thumbnail/Thumbnail';
+
+export const ThumbnailWrapper = () => {
+  return (
+    <div>
+      <Thumbnail />
+    </div>
+  );
+};
