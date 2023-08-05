85b95e7bf1e331b40c11cca31277d9be9058cc53
Taehyun Kim <agart319@gmail.com>
2023-08-05 19:47:16 +0900

    update : 상세, 상세댓글페이지 수정

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,128 +1,79 @@
-import { Button } from 'components/common';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-// import {useState} from 'react';
-// import { useQuery } from '@tanstack/react-query';
-// import { getReviewDetail, ReviewDetail } from 'api/detailApi';
+import { useQuery } from '@tanstack/react-query';
+import { getReviewDetail, ReviewDetail } from 'api/detailApi';
 import { useParams } from 'react-router-dom';
-import { NavBar, Footer } from 'components/layout';
-// import { MyFavoriteTags } from 'components/common/Modal/MyFavoriteTags/MyFavoriteTags';
-// import { Tag } from 'components/common/Tag/Tag';
-// import { Input } from 'components/common';
+import { CommonLayout, NavBar } from 'components/layout';
+import { Footer } from 'components/detailPage/Footer/Footer'; // index 오류
 
 export const DetailPage = () => {
-  // const [isModalOpen, setModalOpen] = useState(false);
+  const contentRef = React.useRef<HTMLDivElement>(null);
   const { reviewId } = useParams<{ reviewId: string }>();
-  // if (!reviewId) {
-  //   throw new Error('Review ID is missing');
-  // }
-  // const { data, isLoading, isError, error } = useQuery<ReviewDetail, Error>({
-  //   queryKey: ['reviewDetail', reviewId],
-  //   queryFn: () => getReviewDetail(reviewId),
-  //   enabled: !!reviewId,
-  // });
+  if (!reviewId) {
+    throw new Error('Review ID is missing');
+  }
+  const { data, isLoading, isError, error } = useQuery<ReviewDetail, Error>({
+    queryKey: ['reviewDetail', reviewId],
+    queryFn: () => getReviewDetail(reviewId),
+    enabled: !!reviewId,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
 
-  const data = {
-    id: 1,
-    content: '본문본문이요본민잉본문이요본문ㅇ본문',
-    img_url:
-      'https://images.unsplash.com/photo-1690463230075-ce79152c2c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
-    title: '제목이요제목제목제목',
-    video_url: null,
-    createdAt: '2021-03-21T14:48:00.000Z',
-    nickname: '닉네임',
-    profileImg_url:
-      'https://media.suara.com/suara-partners/semarang/thumbs/336x189/2022/11/22/1-cat-g497210635-1920.jpg',
-    address: '서울시 마포구 매봉산로 18',
-    comments: [
-      {
-        comment_id: 1,
-        username: '닉네11임',
-        profile_img:
-          'https://media.suara.com/suara-partners/semarang/thumbs/336x189/2022/11/22/1-cat-g497210635-1920.jpg',
-        comment: '댓글입11니다',
-      },
-      {
-        comment_id: 2,
-        username: '닉네22임',
-        profile_img:
-          'https://media.suara.com/suara-partners/semarang/thumbs/336x189/2022/11/22/1-cat-g497210635-1920.jpg',
-        comment: '댓글입니22다',
-      },
-      {
-        comment_id: 3,
-        username: '닉네33임',
-        profile_img:
-          'https://media.suara.com/suara-partners/semarang/thumbs/336x189/2022/11/22/1-cat-g497210635-1920.jpg',
-        comment: '댓글입33니다',
-      },
-      {
-        comment_id: 4,
-        username: '닉네44임',
-        profile_img:
-          'https://media.suara.com/suara-partners/semarang/thumbs/336x189/2022/11/22/1-cat-g497210635-1920.jpg',
-        comment: '댓글입니다44',
-      },
-    ],
+  const handleGotoContent = () => {
+    if (contentRef.current) {
+      contentRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
   };
-  const navigate = useNavigate();
   return (
     <>
-      {/* {isLoading && <div>Loading...</div>}
-      {isError && <div>{String(error)}</div>} */}
-      <NavBar btnLeft={'logo'} btnRight={'myPage'}>
-        동네마실
-      </NavBar>
-      <StDetailPageContainer img={data.img_url}>
-        <StDetailPageHeader>
-          <StDetailPageTitle>{data.title}</StDetailPageTitle>
-          <StDetailPageInfo>
-            <StDetailPageNickname>{data.nickname}</StDetailPageNickname>
-            <StDetailPageCreatedAt>{data.createdAt}</StDetailPageCreatedAt>
-          </StDetailPageInfo>
-        </StDetailPageHeader>
-        <StDetailPageContent>
-          <StDetailPageImg src={data.img_url} />
-          <StDetailPageText>{data.content}</StDetailPageText>
-        </StDetailPageContent>
-        <Button
-          type={'normal'}
-          onClick={() => navigate(`/review/comments/${reviewId}`)}
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{String(error)}</div>}
+      {data && (
+        <CommonLayout
+          header={
+            <NavBar btnLeft={'logo'} btnRight={'myPage'}>
+              동네마실
+            </NavBar>
+          }
+          footer={
+            <Footer
+              reviewId={reviewId}
+              likeCnt={data.likeCnt}
+              commentCnt={data.commentCnt}
+              onClick={handleGotoContent}
+            ></Footer>
+          }
         >
-          댓글보기
-        </Button>
-      </StDetailPageContainer>
-      <Footer />
+          <StDetailPageContainer img={data.img_url}>
+            <StDetailPageHeader>
+              <h1>{data.title}</h1>
+              <StDetailPageInfo>
+                <h3>{data.nickname}</h3>
+                <h6>{data.createdAt}</h6>
+              </StDetailPageInfo>
+            </StDetailPageHeader>
+            <StDetailPageContent>
+              <img src="https://source.unsplash.com/random" />
+              <img src="https://source.unsplash.com/random" />
+              <img src="https://source.unsplash.com/random" />
+              <img src={data.img_url} />
+              <p ref={contentRef}>{data.content}</p>
+            </StDetailPageContent>
+          </StDetailPageContainer>
+        </CommonLayout>
+      )}
     </>
   );
 };
 interface StDetailPageContainerProps {
   img: string;
 }
-// export const StDetailPageContainer = styled.div<StDetailPageContainerProps>`
-//   width: 100%;
-//   height: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   position: relative;
-//   background-color: black;
-//   z-index: 1;
-//   ::before {
-//     content: '';
-//     position: absolute;
-//     top: 0;
-//     right: 0;
-//     bottom: 0;
-//     left: 0;
-//     background-image: ${(props) => `url(${props.img})`};
-//     background-size: cover;
-//     background-position: center;
-//     background-repeat: no-repeat;
-//     filter: blur(10px) brightness(0.5);
-//     z-index: -1;
-//   }
-// `;
 
 export const StDetailPageContainer = styled.div<StDetailPageContainerProps>`
   width: 100%;
@@ -138,46 +89,41 @@ export const StDetailPageHeader = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-
-export const StDetailPageTitle = styled.div`
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 10px;
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
 `;
 
 export const StDetailPageInfo = styled.div`
   display: flex;
   justify-content: flex-end;
-`;
-
-export const StDetailPageNickname = styled.div`
-  font-size: 16px;
-  font-weight: bold;
-  margin-right: 10px;
-`;
-
-export const StDetailPageCreatedAt = styled.div`
-  font-size: 16px;
+  h3 {
+    font-size: 16px;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  h6 {
+    font-size: 16px;
+  }
 `;
 
 export const StDetailPageContent = styled.div`
   width: 100%;
-  height: 400px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-
-export const StDetailPageImg = styled.img`
-  width: 100%;
-  height: 300px;
-  object-fit: cover;
-`;
-
-export const StDetailPageText = styled.div`
-  width: 100%;
-  height: 100px;
-  font-size: 16px;
-  line-height: 1.5;
+  gap: 0.3rem;
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+  }
+  p {
+    width: 100%;
+    height: 100px;
+    font-size: 16px;
+    line-height: 1.5;
+  }
 `;
