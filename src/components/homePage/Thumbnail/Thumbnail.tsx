7a0cc13bc12c52a89e90c02daee7dc60d4d5ad15
Sujin Lee <soolovepat@naver.com>
2023-08-05 10:53:50 +0900

    update : 더미데이터 값 받기

--- src/components/homePage/Thumbnail/Thumbnail.tsx
+++ src/components/homePage/Thumbnail/Thumbnail.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StThumbnail } from './Thumbnail.styles';
 
-export const Thumbnail = () => {
+interface ThumbnailProps {
+  road_name: string;
+  img_url: string;
+  tag: string[];
+  likeCnt: string;
+}
+
+export const Thumbnail = ({
+  road_name,
+  img_url,
+  tag,
+  likeCnt,
+}: ThumbnailProps) => {
   return (
     <StThumbnail>
-      <span>양화로</span>
+      <img src={img_url} />
+      <span>{road_name}</span>
       <span>하트</span>
-      <span>203</span>
+      <span>{likeCnt}</span>
       <span>날씨가 좋아 생각 정리 겸 산책에 나갔다.</span>
+      <span>{tag}</span>
     </StThumbnail>
   );
 };
--- src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.tsx
+++ src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.tsx
@@ -2,9 +2,69 @@ import React from 'react';
 import { Thumbnail } from '../Thumbnail/Thumbnail';
 
 export const ThumbnailWrapper = () => {
+  const result = [
+    {
+      id: 1,
+      road_name: '서울숲로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 2,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 3,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 4,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 5,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 6,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+    {
+      id: 7,
+      road_name: '한강로',
+      img_url: 'img',
+      tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+      likeCnt: '1354',
+    },
+  ];
+
   return (
     <div>
-      <Thumbnail />
+      {result.map((post) => (
+        <Thumbnail
+          key={post.id}
+          road_name={post.road_name}
+          img_url={post.img_url}
+          tag={post.tag}
+          likeCnt={post.likeCnt}
+        />
+      ))}
     </div>
   );
 };
