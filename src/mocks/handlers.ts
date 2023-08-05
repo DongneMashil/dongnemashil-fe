a4bd4137b2c53cd63bfed27d68eee9616f94cce0
Sujin Lee <soolovepat@naver.com>
2023-08-05 23:14:55 +0900

    create : 더미데이터 및 MSW 서버 handler 생성

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,47 @@
+import { ThumbnailProps } from 'components/homePage';
+import { rest } from 'msw';
+
+export interface PaginationResponse<T> {
+  contents: T[];
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalCount: number;
+  isLastPage: boolean;
+  isFirstPage: boolean;
+}
+
+export const result = Array.from(Array(1000).keys()).map(
+  (id): ThumbnailProps => ({
+    id,
+    road_name: '서울숲로',
+    img_url: 'https://source.unsplash.com/random',
+    tag: ['반려동물과 함께', '탁 트인 풍경', '자연과 함께'],
+    likeCnt: `12${id}`,
+  })
+);
+
+export const handlers = [
+  rest.get('/reviews', async (req, res, ctx) => {
+    console.log('MSW: Intercepted GET request to /reviews');
+    // const { searchParams } = req.url;
+    const size = 10;
+    const page = 0;
+    const totalCount = result.length;
+    const totalPages = Math.round(totalCount / size);
+
+    return res(
+      ctx.status(200),
+      ctx.json<PaginationResponse<ThumbnailProps>>({
+        contents: result.slice(page * size, (page + 1) * size),
+        pageNumber: page,
+        pageSize: size,
+        totalPages,
+        totalCount,
+        isLastPage: totalPages <= page,
+        isFirstPage: page === 0,
+      }),
+      ctx.delay(500)
+    );
+  }),
+];
