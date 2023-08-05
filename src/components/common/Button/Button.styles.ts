1ff45049157ba22c1b227092267d01e689c47543
Sujin Lee <soolovepat@naver.com>
2023-08-05 23:53:50 +0900

    design : 스타일 추가

--- src/components/common/Button/Button.styles.ts
+++ src/components/common/Button/Button.styles.ts
@@ -2,10 +2,13 @@ import { styled } from 'styled-components';
 import { ButtonProps } from './Button';
 
 export const StButton = styled.button<ButtonProps>`
+  background: none;
   &.icon {
+    font-size: 20px;
   }
 
   &.normal {
+    font-size: 20px;
   }
 
   &.circle {
