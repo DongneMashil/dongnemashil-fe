1c1cea9bf23b4cf23a91b43a4463a71e2d30d492
Sujin Lee <soolovepat@naver.com>
2023-08-05 09:24:32 +0900

    create : 산책로 썸네일 component 생성

---
+++ src/components/homePage/Thumbnail/Thumbnail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StThumbnail } from './Thumbnail.styles';
+
+export const Thumbnail = () => {
+  return (
+    <StThumbnail>
+      <span>양화로</span>
+      <span>하트</span>
+      <span>203</span>
+      <span>날씨가 좋아 생각 정리 겸 산책에 나갔다.</span>
+    </StThumbnail>
+  );
+};
