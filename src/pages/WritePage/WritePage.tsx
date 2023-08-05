37369ddab43ed3455b553a9564add3c9c12f89b8
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-05 13:15:10 +0900

    done: 이미지, 동영상 불러오기

--- src/pages/WritePage/WritePage.tsx
+++ src/pages/WritePage/WritePage.tsx
@@ -13,6 +13,8 @@ export const WritePage = () => {
     content: '',
   });
   const [images, setImages] = useState<string[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -22,32 +24,58 @@ export const WritePage = () => {
     });
   };
 
-  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(e.target.files || []);
-    const imageFiles = files.filter(file => file.type.startsWith('image/'));
-  
-    if (imageFiles.length > 5) {
-      alert('You can select up to 5 images.');
+    const imageFiles = files.filter((file) => file.type.startsWith('image/'));
+    const videoFiles = files.filter((file) => file.type.startsWith('video/'));
+
+    const currentVideoCount = images.filter((img) =>
+      img.startsWith('data:video')
+    ).length;
+    const currentImageCount = images.length - currentVideoCount;
+
+    if (
+      currentImageCount +
+        currentVideoCount +
+        imageFiles.length +
+        videoFiles.length >
+      5
+    ) {
+      alert('이미지와 동영상의 합은 최대 5개까지 가능합니다.😱');
       return;
     }
 
-    const newImages = imageFiles.map(file => {
-      return new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = function() {
-          resolve(reader.result as string);
-        }
-        reader.onerror = reject;
-        reader.readAsDataURL(file);
-      });
-    });
+    if (currentVideoCount + videoFiles.length > 1) {
+      alert('비디오는 한개만 가능합니다.😱');
+      return;
+    }
 
-    Promise.all(newImages).then(setImages);
+    const newItems: string[] = [];
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        newItems.push(reader.result as string);
+
+        if (newItems.length === files.length) {
+          setImages((prevImages) => [...prevImages, ...newItems]);
+        }
+      };
+      reader.readAsDataURL(file);
+    });
   };
 
   const onButtonClick = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
     fileInputRef.current?.click();
+  };
+
+  const onSlideButtonHandeler = (direction: 'left' | 'right') => {
+    if (direction === 'left') {
+      setCurrentPage((prev) => (prev === 0 ? images.length : prev - 1));
+    } else {
+      setCurrentPage((prev) => (prev === images.length ? 0 : prev + 1));
+    }
   };
 
   return (
@@ -59,21 +87,39 @@ export const WritePage = () => {
         value={formValues.title}
         onChange={onInputChange}
       />
-      <div>
-        <div>이미지</div>
-        <div>
-          <StPlusButton onClick={onButtonClick}>+</StPlusButton>
-          <StHiddenButton
-            ref={fileInputRef}
-            type="file"
-            accept="image/*, video/*"
-            multiple
-            onChange={onFileChange}
+      <div style={{ position: 'relative', minHeight: '200px' }}>
+        <button onClick={() => onSlideButtonHandeler('left')}>Prev</button>
+
+        {/* 현재 페이지가 images.length일 경우 +버튼을 보여줍니다. */}
+        {currentPage === images.length ? (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }}
+          >
+            <StPlusButton onClick={onButtonClick}>+</StPlusButton>
+          </div>
+        ) : images[currentPage].startsWith('data:image') ? (
+          <img
+            src={images[currentPage]}
+            alt={`Upload Preview ${currentPage}`}
           />
-        </div>
-        {images.map((image, index) => (
-          <img src={image} key={index} alt={`Upload Preview ${index}`} />
-        ))}
+        ) : (
+          <video src={images[currentPage]} controls />
+        )}
+
+        <button onClick={() => onSlideButtonHandeler('right')}>Next</button>
+
+        <StHiddenButton
+          ref={fileInputRef}
+          type="file"
+          accept="image/*, video/*"
+          multiple
+          onChange={onFileChange}
+        />
       </div>
       <textarea />
     </>
