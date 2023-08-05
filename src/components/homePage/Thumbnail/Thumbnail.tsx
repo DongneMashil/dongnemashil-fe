c0712999383906e5d84d5aaa7512c3f281c1fd4c
Sujin Lee <soolovepat@naver.com>
2023-08-05 12:10:05 +0900

    update : 썸네일 클릭 이벤트 추가

--- src/components/homePage/Thumbnail/Thumbnail.tsx
+++ src/components/homePage/Thumbnail/Thumbnail.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StThumbnail } from './Thumbnail.styles';
 import { Span } from '../Span/Span';
+import { useNavigate } from 'react-router-dom';
 
 interface ThumbnailProps {
+  key: number;
   road_name: string;
   img_url: string;
   tag: string[];
@@ -10,17 +12,24 @@ interface ThumbnailProps {
 }
 
 export const Thumbnail = ({
+  key,
   road_name,
   img_url,
   tag,
   likeCnt,
 }: ThumbnailProps) => {
+  const navigate = useNavigate();
+
+  const onClickThumbnail = () => {
+    navigate(`/review/${key}`);
+  };
+
   return (
-    <StThumbnail>
+    <StThumbnail onClick={onClickThumbnail}>
       <img src={img_url} />
       <div>
         <Span size={'title'}>{road_name}</Span>
-        <Span>하트 {likeCnt}</Span>
+        <Span>좋아요 {likeCnt}</Span>
         <Span>{tag}</Span>
       </div>
     </StThumbnail>
