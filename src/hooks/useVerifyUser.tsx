7a765b14e243b502adb6f4ec13e159e7afd1a762
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-08 14:07:34 +0900

    done: useVerifyUser 기능 수정 (쿼리 실행 제어하는 bool값 추가)

--- src/hooks/useVerifyUser.tsx
+++ src/hooks/useVerifyUser.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { verifyUser } from 'api/loginApi';
 import { useSetRecoilState } from 'recoil';
 import { UserState, userProfileSelector } from 'recoil/userExample';
@@ -8,12 +8,15 @@ export interface tempUserState {
   nickname: string;
 }
 
+/** 로그인 여부가 필요할 때 사용하는 cusom hook
+ * @param {boolean} shouldVerify - useQuery 실행 여부 제어하는 값
+ */
 export const useVerifyUser = (shouldVerify: boolean) => {
   //const queryClient = useQueryClient();
   const setUserState = useSetRecoilState(userProfileSelector);
 
   return useQuery<tempUserState, Error>({
-    queryKey: 'isUserValid',
+    queryKey: ['isUserValid'],
     queryFn: () => verifyUser(),
     enabled: shouldVerify,
     onSuccess: (data: tempUserState) => {
