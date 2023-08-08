06c895939896c9207ef6a2d43918480f54f72a2d
Taehyun Kim <agart319@gmail.com>
2023-08-08 12:22:40 +0900

    update : 탭 구성 테스트 data 작성

--- src/components/common/myPage/TabMenu/TabMenu.tsx
+++ src/components/common/myPage/TabMenu/TabMenu.tsx
@@ -2,38 +2,77 @@ import React, { useCallback, useState } from 'react';
 import { styled } from 'styled-components';
 import { TabButton } from '../TabButton/TabButton';
 export const TabMenu = () => {
-  const [isLeftSelected, setIsLeftSelected] = useState(true);
+  const [isMyReviewSelected, setIsMyReviewSelected] = useState(true);
   const onTabClickHandler = useCallback(() => {
-    setIsLeftSelected(!isLeftSelected);
-  }, [isLeftSelected]);
+    setIsMyReviewSelected(!isMyReviewSelected);
+  }, [isMyReviewSelected]);
 
   return (
     <StTabContainer>
       <StTabWrapper>
         <TabButton
-          selected={isLeftSelected}
-          onClick={!isLeftSelected ? onTabClickHandler : undefined}
+          selected={isMyReviewSelected}
+          onClick={!isMyReviewSelected ? onTabClickHandler : undefined}
         >
           내가 쓴 글
         </TabButton>
         <TabButton
-          selected={!isLeftSelected}
-          onClick={isLeftSelected ? onTabClickHandler : undefined}
+          selected={!isMyReviewSelected}
+          onClick={isMyReviewSelected ? onTabClickHandler : undefined}
         >
           내가 쓴 댓글
         </TabButton>
       </StTabWrapper>
-      <StTabContentBox></StTabContentBox>
+      <StTabContentBox>
+        {isMyReviewSelected ? (
+          <>
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+            <img src="https://picsum.photos/200/300" alt="img" />
+          </>
+        ) : (
+          <>
+            <img src="https://source.unsplash.com/random" alt="img" />
+            <img src="https://source.unsplash.com/random" alt="img" />
+            <img src="https://source.unsplash.com/random" alt="img" />
+            <img src="https://source.unsplash.com/random" alt="img" />
+            <img src="https://source.unsplash.com/random" alt="img" />
+            <img src="https://source.unsplash.com/random" alt="img" />
+          </>
+        )}
+      </StTabContentBox>
     </StTabContainer>
   );
 };
 
 export const StTabContentBox = styled.div`
   width: 100%;
-  background-color: red;
-  height: 100%;
+  background-color: #fff;
+  padding-top: 1rem;
   top: 0;
-  z-index: 10;
+  z-index: 100;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+  gap: 5px;
+
+  @media (min-width: 300px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+  }
 `;
 
 export const StTabContainer = styled.div`
