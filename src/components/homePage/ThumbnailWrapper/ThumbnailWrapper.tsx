57ab1cc1b8d456c7e6970033c14e16e9ae0dceae
soolovepat <soolovepat@naver.com>
2023-08-09 22:32:53 +0900

    update : useIntersect 커스텀 훅 적용

--- src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.tsx
+++ src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.tsx
@@ -1,52 +1,34 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Thumbnail } from '../Thumbnail/Thumbnail';
-import { StThumbnailWrapper } from './ThumbnailWrapper.styles';
-import { ResponseData, ReviewsList } from 'api/reviewsApi';
-import { axiosInstance } from 'api/api';
-
-// const PAGE_SIZE = 1000;
+import { StTarget, StThumbnailWrapper } from './ThumbnailWrapper.styles';
+import { useFetchReviews } from 'api/reviewsApi';
+import { useIntersect } from 'hooks/useIntersect';
 
 export const ThumbnailWrapper = () => {
   const type = 'likes';
-  const [page, setPage] = useState(1);
-  const [reviews, setReviews] = useState<ReviewsList[]>([]);
-  const [isFetching, setFetching] = useState(false);
-  const [hasNextPage, setNextPage] = useState(true);
+  const [page] = useState(1);
 
-  const fetchReviews = useCallback(async () => {
-    const { data } = await axiosInstance.get<ResponseData>('/reviews', {
-      params: { type, page },
-    });
-    setReviews(reviews.concat(data.content));
-    setPage(page + 1);
-    setNextPage(!data.last);
-    setFetching(false);
-    console.log(data.content);
-  }, [page]);
+  const { data, hasNextPage, isFetching, fetchNextPage } = useFetchReviews({
+    type: type,
+  });
+
+  console.log(data);
+
+  const reviews = useMemo(
+    () => (data ? data.pages.flatMap(({ data }) => data.content) : []),
+    [data]
+  );
 
   console.log(page);
   console.log(isFetching);
   console.log(hasNextPage);
-  console.log('innerHeight :', window.innerHeight);
-  console.log('scrollTop :', document.documentElement.scrollTop);
-  console.log('offsetHeight :', document.documentElement.offsetHeight);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, offsetHeight } = document.documentElement;
-      if (window.innerHeight + scrollTop >= offsetHeight) {
-        setFetching(true);
-      }
-    };
-    setFetching(true);
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  useEffect(() => {
-    if (isFetching && hasNextPage) fetchReviews();
-    else if (!hasNextPage) setFetching(false);
-  }, [isFetching]);
+  const ref = useIntersect((entry, observer) => {
+    observer.unobserve(entry.target);
+    if (hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  });
 
   return (
     <StThumbnailWrapper>
@@ -55,14 +37,15 @@ export const ThumbnailWrapper = () => {
           key={review.id}
           id={review.id}
           roadName={review.roadName}
-          img_url={review.img_url}
+          mainImgUrl={review.mainImgUrl}
+          videoUrl={review.videoUrl}
+          profileImgUrl={review.profileImgUrl}
           likeCnt={review.likeCnt}
-          tag={review.tag}
-          profileImg_url={review.profileImg_url}
+          likebool={review.likebool}
         />
       ))}
       {isFetching && <div>Loading...</div>}
-      <div style={{ position: 'fixed', bottom: 10, right: 10 }}></div>
+      <StTarget ref={ref} />
     </StThumbnailWrapper>
   );
 };
