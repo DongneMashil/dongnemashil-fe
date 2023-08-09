f7cd8c74fe50e5455d7085b992ee73c30ccb6f5a
soolovepat <soolovepat@naver.com>
2023-08-09 23:08:48 +0900

    update : API 명세서 변경에 의한 데이터 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { ReviewsList } from 'api/reviewsApi';
+import { user } from 'assets/user';
 import { rest } from 'msw';
 
 export interface PaginationResponse<T> {
@@ -18,7 +19,7 @@ export const result = Array.from(Array(1000).keys()).map(
     mainImgUrl: 'https://source.unsplash.com/random',
     videoUrl:
       'https://dongnemashil-image.s3.ap-northeast-2.amazonaws.com/f8b850ad-a2b6-4f56-a1e2-a6b5c8dc382c-KakaoTalk_Video_2023-08-09-15-47-34.mp4',
-    profileImgUrl: 'https://source.unsplash.com/random',
+    profileImgUrl: user,
     likeCnt: 1200,
     likebool: false,
   })
@@ -28,10 +29,9 @@ export const handlers = [
   rest.get('/reviews', async (req, res, ctx) => {
     console.log('MSW: Intercepted GET request to /reviews');
     // const { searchParams } = req.url;
-    // const size = Number(searchParams.get('size'));
-    // const page = Number(searchParams.get('page'));
-    const size = 10;
+    // const type = searchParams.get('type');
     const page = 0;
+    const size = 10;
     const totalCount = result.length;
     const totalPages = Math.round(totalCount / size);
 
