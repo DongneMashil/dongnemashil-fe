6658c424998a98ada365bc76416d5f1a0c7741d5
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-09 11:20:48 +0900

    feat: SearchPage 레이아웃 스타일링 시작

--- src/pages/SearchPage/SearchPage.styles.ts
+++ src/pages/SearchPage/SearchPage.styles.ts
@@ -1,1 +1,26 @@
 import styled from 'styled-components';
+import { StInput } from 'components/common/Input/Input.styles';
+
+export const StSearchHeader = styled.div`
+  border-bottom: 1px solid #e3e3e3;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    /* & > svg:first-child {
+      margin-right: auto;
+    } */
+  }
+`;
+
+export const StSearchInput = styled(StInput)`
+  width: 166px;
+  border-radius: 25px;
+  margin: 12px 0;
+`;
