69d0accb7ce1eef3e8d0cc2f2e3ad73bbbfafa1f
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-09 11:27:53 +0900

    chore: axios 인스턴스 불필요한 주석 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,11 +9,6 @@ export const axiosInstance: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
-/** 유저 인증 인스턴스 - 로그인 상태의 api 요청시 해당 인스턴스 사용 필요 */
-// export const axiosUserInstance: AxiosInstance = axios.create({
-//   baseURL: baseUrl,
-//   withCredentials: true,
-// });
 /** access token 갱신 interceptor */
 axiosInstance.interceptors.response.use(
   (res: AxiosResponse): AxiosResponse => {
@@ -25,19 +20,3 @@ axiosInstance.interceptors.response.use(
     getNewAccessToken(); // 리프레시 토큰으로 갱신!!!
   }
 );
-
-// const verifyUserSuccess = async (
-//   config: AxiosRequestConfig
-// ): Promise<AxiosRequestConfig> => {
-//   return config; // 20X 정상 응답시 그냥 전달
-// };
-
-// const verifyUserError = err : any => {
-//   return Promise.reject(err);
-// }
-
-/** access token 인증 실패시 사용 인스턴스 (refresh token 갱신용) */
-// export const axiosRefreshToken: AxiosInstance = axios.create({
-//   baseURL: baseUrl,
-//   withCredentials: true,
-// });
