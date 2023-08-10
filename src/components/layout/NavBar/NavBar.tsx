cd964474a09193bd5280cc73e9383ebae04b87c8
soolovepat <soolovepat@naver.com>
2023-08-10 10:38:35 +0900

    update : NavBar 오른쪽 두번째 버튼 추가

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -2,14 +2,21 @@ import { Button } from 'components/common';
 import React from 'react';
 import { StNavBar } from './NavBar.styles';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Search } from 'assets/icons/Search.svg';
 
 export interface NavBarProps {
-  children: React.ReactNode;
+  children?: React.ReactNode | null;
   btnLeft?: 'logo' | 'back';
+  btnSecondRight?: 'search' | null;
   btnRight?: 'close' | 'mypage' | 'submit' | null;
 }
 
-export const NavBar = ({ btnLeft, btnRight, children }: NavBarProps) => {
+export const NavBar = ({
+  btnLeft,
+  btnSecondRight,
+  btnRight,
+  children,
+}: NavBarProps) => {
   const navigate = useNavigate();
 
   const goBack = () => {
@@ -31,21 +38,28 @@ export const NavBar = ({ btnLeft, btnRight, children }: NavBarProps) => {
           ⬅️
         </Button>
       )}
-      <div>{children}</div>
-      {btnRight === 'close' ? (
-        <Button type={'icon'} url={'/'}>
-          {/* <img src={'/close.jpg'} /> */}
-          ✖️
-        </Button>
-      ) : btnRight === 'mypage' ? (
-        <Button type={'normal'} url={'/mypage'}>
-          👀
-        </Button>
-      ) : btnRight === 'submit' ? (
-        <Button type={'normal'} onClick={onClickSubmit}>
-          Submit
-        </Button>
-      ) : null}
+      {children ? <div>{children}</div> : null}
+      <div>
+        {btnSecondRight === 'search' ? (
+          <Button type={'icon'} url={'/search'}>
+            <Search />
+          </Button>
+        ) : null}
+        {btnRight === 'close' ? (
+          <Button type={'icon'} url={'/'}>
+            {/* <img src={'/close.jpg'} /> */}
+            ✖️
+          </Button>
+        ) : btnRight === 'mypage' ? (
+          <Button type={'normal'} url={'/mypage'}>
+            👀
+          </Button>
+        ) : btnRight === 'submit' ? (
+          <Button type={'normal'} onClick={onClickSubmit}>
+            Submit
+          </Button>
+        ) : null}
+      </div>
     </StNavBar>
   );
 };
