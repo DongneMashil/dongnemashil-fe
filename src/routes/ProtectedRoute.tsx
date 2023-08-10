8cc417c63280f8c903356fce150ec3d94f9fa1fa
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-10 16:02:31 +0900

    udpate: Protected route 리코일로 변경

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-// import { useRecoilValue } from 'recoil';
-// import { userIsLoggedInSelector } from 'recoil/userExample';
-import { useVerifyUser } from 'hooks';
+import { useRecoilValue } from 'recoil';
+import { userIsLoggedInSelector } from 'recoil/userExample';
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
-  const { isSuccess } = useVerifyUser(true);
-  return isSuccess ? element : <Navigate to="/login" replace />;
+  const isLoggedIn = useRecoilValue(userIsLoggedInSelector);
+  return isLoggedIn ? element : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
