c881dc4a6b1621d9e907f72f26cd06a85536cb65
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-11 00:24:53 +0900

    design: 검색 페이지 돋보기 아이콘 변경

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { Button } from 'components/common';
-import { ReactComponent as Search } from 'assets/icons/Search.svg';
+import { ReactComponent as SearchFlat } from 'assets/icons/SearchFlat.svg';
 import { StSearchInput, StSearchHeader } from './SearchPage.styles';
+import { CommonLayout } from 'components/layout';
+import { HeaderText } from 'components/layout/HeaderText/HeaderText';
 
 export const SearchPage = () => {
   const [value, setValue] = useState('');
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
+  const onSubmitHandler = (e: React.ChangeEvent<HTMLInputElement>) => {};
   return (
-    <div>
+    <CommonLayout>
       <StSearchHeader>
         <div>
-          <Search width={16} height={16} fill="none" />
-          <h3>구를 입력해주세요!</h3>
+          <SearchFlat width={16} height={16} />
+          <HeaderText type="h1">구를 입력해주세요!</HeaderText>
         </div>
         <StSearchInput
           type="text"
@@ -25,6 +28,6 @@ export const SearchPage = () => {
         />
       </StSearchHeader>
       <Button>test용 전송버튼</Button>
-    </div>
+    </CommonLayout>
   );
 };
