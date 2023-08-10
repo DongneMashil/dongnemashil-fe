0018975a946bca051ecba6babf992049207023f3
soolovepat <soolovepat@naver.com>
2023-08-10 09:56:12 +0900

    refactor : 불필요한 파일 삭제

--- src/assets/tags/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as date } from './date.svg';
-export { default as quiet } from './quiet.svg';
-export { default as animalFriends } from './animal_friends.svg';
-export { default as withBaby } from './with_baby.svg';
-export { default as shade } from './shade.svg';
-export { default as bicycle } from './bicycle.svg';
-export { default as photoPlace } from './photo_place.svg';
-export { default as toilet } from './toilet.svg';
-export { default as rainyDay } from './rainy_day.svg';
