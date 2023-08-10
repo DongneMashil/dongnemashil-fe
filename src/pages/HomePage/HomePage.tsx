7638af0350a52d67eb54b732419c43280173471b
soolovepat <soolovepat@naver.com>
2023-08-10 10:39:38 +0900

    update : btnSecondRight props 추가

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,38 +1,33 @@
-import MapWrapper from 'components/common/Map/MapWrapper';
-import React, { useCallback, useState, useEffect } from 'react';
-import { Button, Input } from 'components/common';
+import React from 'react';
+// import React, { useCallback, useState, useEffect } from 'react';
 import { CommonLayout, NavBar } from 'components/layout';
 import { ThumbnailWrapper } from 'components/homePage';
-import { getNewAccessToken } from 'api/loginApi';
-import { useVerifyUser } from 'hooks';
+// import { useVerifyUser } from 'hooks';
 
 export const HomePage = () => {
-  const [shouldVerify, setShouldVerify] = useState(false);
+  // const [shouldVerify, setShouldVerify] = useState(false);
 
-  const { data } = useVerifyUser(shouldVerify);
+  // const { data } = useVerifyUser(shouldVerify);
 
-  const onVerifyHandler = useCallback(() => {
-    setShouldVerify(true);
-  }, []);
+  // const onVerifyHandler = useCallback(() => {
+  //   setShouldVerify(true);
+  // }, []);
 
-  useEffect(() => {
-    if (data) console.log(data);
-  }, []);
+  // useEffect(() => {
+  //   if (data) console.log(data);
+  // }, []);
 
   return (
     <CommonLayout
       header={
-        <NavBar btnLeft={'logo'} btnRight={'mypage'}>
-          <Input />
-        </NavBar>
+        <NavBar
+          btnLeft={'logo'}
+          btnSecondRight={'search'}
+          btnRight={'mypage'}
+        ></NavBar>
       }
     >
       <ThumbnailWrapper />
-      <div style={{ display: 'flex', gap: '15px', marginTop: '30px' }}>
-        <Button onClick={onVerifyHandler}>Verify User</Button>
-        <Button onClick={getNewAccessToken}>Get New Access Token</Button>
-      </div>
-      <MapWrapper />
     </CommonLayout>
   );
 };
