d1fb0c9333e69b015c7443e82e2f53b4b35bce34
Taehyun Kim <agart319@gmail.com>
2023-08-11 20:12:45 +0900

    update : browser-image-compression 설치,프로필사진 업로드 기능

--- src/pages/MyProfilePage/MyProfilePage.tsx
+++ src/pages/MyProfilePage/MyProfilePage.tsx
@@ -2,17 +2,20 @@ import { useQuery } from '@tanstack/react-query';
 import { MyProfile, getMyProfile } from 'api/mypageApi';
 import { CommonLayout, NavBar } from 'components/layout';
 import { useVerifyUser } from 'hooks';
-// import React, { useEffect, useState } from 'react';
-import { useEffect } from 'react';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userProfileSelector } from 'recoil/userExample';
 import { styled } from 'styled-components';
 import noUser from 'assets/noImage/nouser.gif';
+import imageCompression from 'browser-image-compression';
+import { Button, Input } from 'components/common';
 export const MyProfilePage = () => {
   const userState = useRecoilValue(userProfileSelector);
   const { data: userData } = useVerifyUser(true);
-  // const [fileUrl, setFileUrl] = useState(null);
+  const [fileUrl, setFileUrl] = useState<string | null | undefined>(null);
+  const fileUpload = useRef();
+  const [postData, setPostData] = useState<{ imgUrl?: File }>({});
+  // const [input, setInput] = useState({});
   useEffect(() => {
     console.log('current user state: ', userState);
     if (userData) {
@@ -26,38 +29,78 @@ export const MyProfilePage = () => {
     // enabled: !!userData?.nickname,
     onSuccess: (data) => {
       console.log(data);
-      // setFileUrl(data.profileImgUrl);
+      setFileUrl(data.profileImgUrl);
     },
     onError: (error) => {
       console.log('🔴' + error);
     },
   });
+  console.log(data);
+  // ⬇️ 이미지 압축 옵션
+  const options = {
+    maxSizeMB: 0.8,
+    maxWidthOrHeight: 1020,
+    useWebWorker: true,
+  };
+  // ⬇️ 이미지 압축
+  const onChangeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = e.target.files?.[0];
+    if (!imageFile) return;
+    try {
+      const compressedFile = await imageCompression(imageFile, options);
+      const imgUrl = URL.createObjectURL(compressedFile);
+      setFileUrl(imgUrl);
+      setPostData({ imgUrl: compressedFile });
+      console.log(postData + '이미지 압축');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <CommonLayout header={<NavBar>회원정보수정</NavBar>}>
+    <CommonLayout header={<NavBar>회원정보수정</NavBar>} backgroundColor="#fff">
       <StMyProfileContainer>
         <StProfileImage>
-          <img src={data?.profileImgUrl || noUser} alt="프로필 이미지" />
-          {/* <label htmlFor="file" className="pcload">
-            PC에서 불러오기
+          <img src={fileUrl || noUser} alt="프로필 이미지" />
+          <label htmlFor="file" className="pcload">
+            사진 수정
           </label>
           <input
             type="file"
             id="file"
             accept="image/*"
             onChange={onChangeImage}
-            ref={fileUpload}
-          /> */}
-          <button type="button">프로필 사진 변경</button>
+            ref={fileUpload.current}
+          />
         </StProfileImage>
+        <StNickNameTitle>닉네임</StNickNameTitle>
+        <StNickNameWrapper>
+          <Input type="text" placeholder="닉네임" />
+          <Button type="normal">중복확인</Button>
+        </StNickNameWrapper>
       </StMyProfileContainer>
     </CommonLayout>
   );
 };
 
+const StNickNameTitle = styled.div`
+  margin-top: 40px;
+  width: 100%;
+  color: var(--strokepurple, #9a7b9a);
+  font-family: Pretendard;
+`;
+
+const StNickNameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 40px;
+  width: 100%;
+`;
 const StMyProfileContainer = styled.div`
   width: 100%;
-  height: 100%;
-  background-color: #ffffff;
+
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -76,5 +119,25 @@ const StProfileImage = styled.div`
     height: 84px;
     border-radius: 50px;
     object-fit: cover;
+  }
+  label {
+    color: var(--strokepurple, #9a7b9a);
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    cursor: pointer;
+    height: 40px;
+  }
+
+  input[type='file'] {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 0;
   }
 `;
