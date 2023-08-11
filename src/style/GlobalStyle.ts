f95c1e0ab84ede007af55482d0f269ad42b62907
Taehyun Kim <agart319@gmail.com>
2023-08-11 10:21:23 +0900

    fix : globalstyle 폰트관련 버그, commonstyle 높이 관련 버그 수정

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css');
 
 * { box-sizing: border-box; }
 html, body, div, span, applet, object, iframe,
