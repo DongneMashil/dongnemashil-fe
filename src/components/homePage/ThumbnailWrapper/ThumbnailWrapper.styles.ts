58fc11d51b44ad0ae0620c0b9fb5619489e42ccf
soolovepat <soolovepat@naver.com>
2023-08-11 09:50:15 +0900

    design : 스타일 수정

--- src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.styles.ts
+++ src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.styles.ts
@@ -1,8 +1,12 @@
+import { theme } from 'style/theme';
 import { styled } from 'styled-components';
 
 export const StThumbnailWrapper = styled.div`
-  width: 92%;
-  margin: 0 auto;
+  width: 100%;
+  padding: 0 15px;
+  border-radius: 14px;
+  background-color: ${theme.whiteColor};
+  box-shadow: 0px -2px 10px 0px rgba(0, 0, 0, 0.06);
 `;
 
 export const StTarget = styled.div`
