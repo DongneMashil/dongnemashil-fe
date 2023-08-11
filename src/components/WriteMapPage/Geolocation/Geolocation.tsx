a33f549e1280c3a16f4914cdcdd8a1ad1f617a9f
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-11 17:37:53 +0900

    done: 현재 위치 가져오기

--- src/components/WriteMapPage/Geolocation/Geolocation.tsx
+++ src/components/WriteMapPage/Geolocation/Geolocation.tsx
@@ -1,62 +1,84 @@
-import React, { useEffect, useRef } from 'react';
+import Map from 'components/common/Map/Map';
+import React from 'react';
 
 interface IProps {
   onMarkerClick?: () => void;
+  onAddressUpdate?: (address: string) => void;
 }
 
-export const Geolocation: React.FC<IProps> = ({ onMarkerClick }) => {
-  const mapRef = useRef<HTMLDivElement>(null);
+interface IMarkerOptions {
+  map: kakao.maps.Map;
+  position: kakao.maps.LatLng;
+  draggable: boolean;
+}
 
-  useEffect(() => {
-    if (!window.kakao || !mapRef.current) return;
-
-    const mapOption = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-      level: 10,
-    };
-
-    const map = new window.kakao.maps.Map(mapRef.current, mapOption);
-
+export const Geolocation: React.FC<IProps> = ({
+  onMarkerClick,
+  onAddressUpdate,
+}) => {
+  const initMap = (map: kakao.maps.Map) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        const locPosition = new window.kakao.maps.LatLng(lat, lon);
+        const locPosition = new kakao.maps.LatLng(lat, lon);
         const message = '<div style="padding:5px;">여기에 계신가요?!</div>';
 
         displayMarker(map, locPosition, message, onMarkerClick);
       });
     } else {
-      const locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+      const locPosition = new kakao.maps.LatLng(37.545043, 127.039245);
       const message = 'geolocation을 사용할 수 없어요..';
 
       displayMarker(map, locPosition, message, onMarkerClick);
     }
-  }, []);
+  };
 
   const displayMarker = (
-    map: any,
-    locPosition: any,
+    map: kakao.maps.Map,
+    locPosition: kakao.maps.LatLng,
     message: string,
     onClick?: () => void
   ) => {
-    const marker = new window.kakao.maps.Marker({
+    const markerOptions: IMarkerOptions = {
       map: map,
       position: locPosition,
-    });
+      draggable: true,
+    };
 
-    const infowindow = new window.kakao.maps.InfoWindow({
+    const marker = new kakao.maps.Marker(markerOptions);
+
+    const infowindow = new kakao.maps.InfoWindow({
       content: message,
       removable: true,
     });
 
-    window.kakao.maps.event.addListener(marker, 'click', function () {
+    kakao.maps.event.addListener(marker, 'click', function () {
       infowindow.open(map, marker);
       if (onClick) onClick();
+    });
+
+    kakao.maps.event.addListener(marker, 'dragend', function () {
+      const position = marker.getPosition();
+
+      new kakao.maps.services.Geocoder().coord2Address(
+        position.getLng(),
+        position.getLat(),
+        function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            const detailAddr = result[0].road_address?.address_name || '';
+            if (onAddressUpdate) {
+              onAddressUpdate(detailAddr); // 주소 업데이트를 위한 콜백 함수 호출
+            }
+          }
+        }
+      );
     });
 
     map.setCenter(locPosition);
   };
 
-  return <div ref={mapRef} style={{ width: '100%', height: '350px' }} />;
+  return (
+      <Map width="100%" height="100%" initMap={initMap} />
+  );
 };
--- src/pages/WriteMapPage/WriteMapPage.tsx
+++ src/pages/WriteMapPage/WriteMapPage.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import MapWrapper from 'components/common/Map/MapWrapper';
+import React, { useState } from 'react';
 import { CommonLayout } from 'components/layout';
 import {
   StCurrentLocationContainer,
   StCurrentLocationText,
   StCurrentLocationTitle,
   StInputWrapper,
-
+  StMarker,
   StPostButton,
 } from './WriteMapPage.styles.ts';
 import { Geolocation } from 'components/writeMapPage';
-// import { ReactComponent as Marker } from 'assets/icons/Marker.svg';
 
 export const WriteMapPage = () => {
+  const [currentAddress, setCurrentAddress] = useState<string>('');
+
   return (
     <CommonLayout>
       <StCurrentLocationContainer>
@@ -20,12 +20,14 @@ export const WriteMapPage = () => {
           산책 지점을 먼저 선택해주세요!
         </StCurrentLocationTitle>
         <StInputWrapper>
-          <Geolocation />
-          <StCurrentLocationText>현위치</StCurrentLocationText>
+          <StMarker />
+          <StCurrentLocationText>
+            현위치: {currentAddress}
+          </StCurrentLocationText>
         </StInputWrapper>
         <StPostButton>글 작성</StPostButton>
       </StCurrentLocationContainer>
-      <MapWrapper />
+      <Geolocation onAddressUpdate={setCurrentAddress} />
     </CommonLayout>
   );
 };
