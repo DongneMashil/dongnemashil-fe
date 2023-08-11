7982d42d38e4d1036cd24f7785b728e02cf65218
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-11 18:39:06 +0900

    create: 검색 결과 지도보기 컴포넌트 생성

---
+++ src/pages/SearchResultMapPage/SearchResultMapPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MapWrapper from 'components/common/Map/MapWrapper';
+import { CommonLayout } from 'components/layout';
+
+export const SearchResultMapPage = () => {
+  return (
+    <CommonLayout>
+      <MapWrapper />
+    </CommonLayout>
+  );
+};
