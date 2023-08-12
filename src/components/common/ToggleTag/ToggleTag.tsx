d7e6dca2dee6389c2491bea5630ff5bd31e0e16f
soolovepat <soolovepat@naver.com>
2023-08-12 15:50:06 +0900

    feat : isSelected 태그 앞으로 정렬 및 코드 리펙토링

--- src/components/common/ToggleTag/ToggleTag.tsx
+++ src/components/common/ToggleTag/ToggleTag.tsx
@@ -6,48 +6,74 @@ interface ToggleTagButtonProps {
   onTagChange?: (selectedTags: string[]) => void;
 }
 
+interface TagProps {
+  id: number;
+  img: string;
+  label: string;
+  isSelected: boolean;
+}
+
 export const ToggleTagButton: React.FC<ToggleTagButtonProps> = ({
   onTagChange,
 }) => {
-  const tags = [
-    { id: 1, img: tagImg.animalFriends, label: '동물친구들' },
-    { id: 2, img: tagImg.withLover, label: '연인이랑' },
-    { id: 3, img: tagImg.bench, label: '벤치' },
-    { id: 4, img: tagImg.quiet, label: '한적해요' },
-    { id: 5, img: tagImg.shade, label: '그늘' },
-    { id: 6, img: tagImg.toilet, label: '화장실' },
-    { id: 7, img: tagImg.photoPlace, label: '사진맛집' },
-    { id: 8, img: tagImg.nature, label: '자연' },
-    { id: 9, img: tagImg.rainyDay, label: '비와도OK' },
-    { id: 10, img: tagImg.nightWalk, label: '밤산책' },
-    { id: 11, img: tagImg.withBaby, label: '아기랑' },
-    { id: 12, img: tagImg.bicycle, label: '자전거' },
+  const initialTags: TagProps[] = [
+    {
+      id: 1,
+      img: tagImg.animalFriends,
+      label: '동물친구들',
+      isSelected: false,
+    },
+    { id: 2, img: tagImg.withLover, label: '연인이랑', isSelected: false },
+    { id: 3, img: tagImg.bench, label: '벤치', isSelected: false },
+    { id: 4, img: tagImg.quiet, label: '한적해요', isSelected: false },
+    { id: 5, img: tagImg.shade, label: '그늘', isSelected: false },
+    { id: 6, img: tagImg.toilet, label: '화장실', isSelected: false },
+    { id: 7, img: tagImg.photoPlace, label: '사진맛집', isSelected: false },
+    { id: 8, img: tagImg.nature, label: '자연', isSelected: false },
+    { id: 9, img: tagImg.rainyDay, label: '비와도OK', isSelected: false },
+    { id: 10, img: tagImg.nightWalk, label: '밤산책', isSelected: false },
+    { id: 11, img: tagImg.withBaby, label: '아기랑', isSelected: false },
+    { id: 12, img: tagImg.bicycle, label: '자전거', isSelected: false },
   ];
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
-  const toggleTag = (tag: string) => {
-    const updatedTags = selectedTags.includes(tag)
-      ? selectedTags.filter((t) => t !== tag)
-      : [...selectedTags, tag];
+  const [tags, setTags] = useState<TagProps[]>(initialTags);
 
-    setSelectedTags(updatedTags);
-    onTagChange?.(updatedTags);
+  const toggleTag = (tag: TagProps) => {
+    const updatedTags = tags.map((t) =>
+      t.id === tag.id ? { ...t, isSelected: !t.isSelected } : t
+    );
+
+    setTags(updatedTags);
+    onTagChange?.(updatedTags.filter((t) => t.isSelected).map((t) => t.label));
   };
 
-  const onTagClick = (tag: string) => () => {
-    toggleTag(tag);
-  };
+  const selectedTags = tags.filter((tag) => tag.isSelected);
+  const unselectedTags = tags.filter((tag) => !tag.isSelected);
 
   return (
     <StTagWrapper>
-      {tags.map((tag) => (
+      {selectedTags.map((tag) => (
         <StTagBox
           key={tag.id}
-          onClick={onTagClick(tag.label)}
-          $isSelected={selectedTags.includes(tag.label)}
+          onClick={() => toggleTag(tag)}
+          $isSelected={true}
         >
-          <img src={tag.img} />
-          <span>{tag.label}</span>
+          <span>
+            <img src={tag.img} alt={tag.label} />
+            <span>{tag.label}</span>
+          </span>
+        </StTagBox>
+      ))}
+      {unselectedTags.map((tag) => (
+        <StTagBox
+          key={tag.id}
+          onClick={() => toggleTag(tag)}
+          $isSelected={false}
+        >
+          <span>
+            <img src={tag.img} alt={tag.label} />
+            <span>{tag.label}</span>
+          </span>
         </StTagBox>
       ))}
     </StTagWrapper>
