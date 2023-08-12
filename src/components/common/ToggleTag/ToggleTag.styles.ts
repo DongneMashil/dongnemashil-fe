4186a43c481b172fd83ce58202b311dea73fa292
soolovepat <soolovepat@naver.com>
2023-08-12 15:50:38 +0900

    design : 태그 style 추가

--- src/components/common/ToggleTag/ToggleTag.styles.ts
+++ src/components/common/ToggleTag/ToggleTag.styles.ts
@@ -1,22 +1,28 @@
 import { styled } from 'styled-components';
 
-export const StTagWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  gap: 0.42rem;
+export const StTagWrapper = styled.ul`
   padding: 1rem;
+  overflow-x: auto;
+  white-space: nowrap;
 `;
 
-export const StTagBox = styled.span<{ $isSelected: boolean }>`
-  border: 1px solid #dedede;
-  height: 1.5rem;
-  line-height: 1.6rem;
-  border-radius: 1rem;
+export const StTagBox = styled.li<{ $isSelected: boolean }>`
+  display: inline-block;
+  white-space: nowrap;
+  margin-right: 10px;
+
+  line-height: 2.2rem;
   font-size: 14px;
-  padding: 0 0.8rem;
   background-color: ${(props) => (props.$isSelected ? '#b5a6ca' : '#FAFAFA')};
+  border: 1px solid #dedede;
+  border-radius: 2.2rem;
+
+  > span {
+    padding: 0 16px;
+
+    img {
+      transform: translateY(4px);
+      margin-right: 6px;
+    }
+  }
 `;
