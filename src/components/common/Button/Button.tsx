f10b293570d8e9589feb5a2affbd9cea997fd5f7
Taehyun Kim <agart319@gmail.com>
2023-08-14 21:29:02 +0900

    update: active프로퍼티 동작 막는 기능 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -38,7 +38,7 @@ export const Button = ({
   $height,
   $round,
   $stroke,
-  $active,
+  $active = true,
 }: ButtonProps) => {
   const navigate = useNavigate();
 
@@ -56,13 +56,13 @@ export const Button = ({
     <StSubmitButton
       type="submit"
       className={type}
-      onClick={handleButtonClick}
+      onClick={$active ? handleButtonClick : () => {}}
       value={value}
     />
   ) : (
     <StButton
       className={type}
-      onClick={handleButtonClick}
+      onClick={$active ? handleButtonClick : () => {}}
       $width={$width}
       $height={$height}
       $round={$round}
