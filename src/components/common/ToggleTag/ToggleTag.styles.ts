e87fde95e4f760e0e03c366f14f9ba422772b3bd
soolovepat <soolovepat@naver.com>
2023-08-14 14:40:31 +0900

    design : scrollbar 숨기기

--- src/components/common/ToggleTag/ToggleTag.styles.ts
+++ src/components/common/ToggleTag/ToggleTag.styles.ts
@@ -6,4 +6,10 @@ export const StTagWrapper = styled.ul`
   white-space: nowrap;
   display: flex;
   align-items: center;
+
+  &::-webkit-scrollbar {
+    display: none; /* 크롬, 사파리, 오페라, 엣지 */
+  }
+  -ms-overflow-style: none; /* 인터넷 익스플로러 */
+  scrollbar-width: none; /* 파이어폭스 */
 `;
