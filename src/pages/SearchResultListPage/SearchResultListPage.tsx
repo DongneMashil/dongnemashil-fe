b7312f3265b2481b8734d96a9e6279f5a8f3aac5
soolovepat <soolovepat@naver.com>
2023-08-14 15:18:15 +0900

    create : SearchResultListPage 생성, 메인페이지 내용 가져오기

---
+++ src/pages/SearchResultListPage/SearchResultListPage.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { CommonLayout, FixFooter, NavBar } from 'components/layout';
+import { ThumbnailWrapper } from 'components/homePage';
+import { ToggleTagButton } from 'components/common/ToggleTag/ToggleTag';
+import { useVerifyUser } from 'hooks';
+import { useRecoilValue } from 'recoil';
+import { userProfileSelector } from 'recoil/userExample';
+
+export const SearchResultListPage = () => {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const handleTagChange = (tags: string[]) => {
+    setSelectedTags(tags);
+  };
+  const userState = useRecoilValue(userProfileSelector);
+  const { data } = useVerifyUser(true);
+
+  useEffect(() => {
+    console.log('current user state: ', userState);
+    if (data) {
+      console.log('useVerifyUser data: ', data);
+    }
+  }, [userState]);
+  console.log(selectedTags);
+
+  return (
+    <CommonLayout
+      header={
+        <>
+          <NavBar
+            btnLeft={'logo'}
+            btnSecondRight={'search'}
+            btnRight={'mypage'}
+          >
+            <h1>서울 전체</h1>
+          </NavBar>
+          <ToggleTagButton onTagChange={handleTagChange} />
+        </>
+      }
+      headerHeight={'150px'}
+      footer={<FixFooter centerButtons={'map'} rightButtons={'goTop'} />}
+    >
+      <ThumbnailWrapper
+        tag={selectedTags.length > 0 ? selectedTags.join(',') : null}
+      />
+    </CommonLayout>
+  );
+};
