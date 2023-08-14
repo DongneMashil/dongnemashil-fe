ff37a4ba1da2d44b7c42633449d9bcc7a09e9426
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-15 01:57:37 +0900

    feat: 기본 api 설정 작업

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -31,3 +31,34 @@ export const submitReview = async (data: globalThis.FormData) => {
     throw e;
   }
 };
+
+export const updateReview = async (
+  reviewId: number,
+  data: globalThis.FormData
+) => {
+  try {
+    const response: AxiosResponse<{ id: number }> = await axiosInstance.put(
+      `/reviews/${reviewId}`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+    );
+    return response.data;
+  } catch (e: unknown) {
+    if (e instanceof AxiosError) {
+      if (e.response?.status === 401) {
+        throw new Error('로그인이 필요한 기능입니다.');
+      } else if (e.response?.status === 403) {
+        throw new Error('작성자가 아닙니다.');
+      } else if (e.response?.status === 404) {
+        throw new Error('리뷰가 존재하지 않습니다.');
+      } else {
+        throw new Error(e.response?.data?.errorMessage || e.message);
+      }
+    }
+    throw e;
+  }
+};
