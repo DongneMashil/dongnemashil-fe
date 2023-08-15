bd61b335185db0822fd3a2248a85a9541bcba1a2
soolovepat <soolovepat@naver.com>
2023-08-15 22:40:37 +0900

    design : 마지막 li 요소 border 0

--- src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.styles.ts
+++ src/components/homePage/ThumbnailWrapper/ThumbnailWrapper.styles.ts
@@ -1,12 +1,16 @@
 import { theme } from 'style/theme';
 import { styled } from 'styled-components';
 
-export const StThumbnailWrapper = styled.div`
+export const StThumbnailWrapper = styled.ul`
   width: 100%;
   padding: 0 15px;
   border-radius: 14px;
   background-color: ${theme.whiteColor};
   box-shadow: 0px -2px 10px 0px rgba(0, 0, 0, 0.06);
+
+  & li:nth-last-child(2) {
+    border-bottom: 0px;
+  }
 `;
 
 export const StTarget = styled.div`
