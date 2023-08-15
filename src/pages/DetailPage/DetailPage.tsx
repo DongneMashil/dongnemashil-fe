8ad4b46a93972717a61fbc01edf4b6454d03a768
Taehyun Kim <agart319@gmail.com>
2023-08-15 18:11:00 +0900

    update: detailPage 버튼 로고-> back으로 변경

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -87,7 +87,7 @@ export const DetailPage = () => {
         <CommonLayout
           header={
             <NavBar
-              btnLeft={'logo'}
+              btnLeft={'back'}
               btnRight={'map'}
               onClickRight={() => setIsMapOpen(true)}
             >
