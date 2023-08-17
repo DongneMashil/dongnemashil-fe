b269785fe863e903028572aba14812a6e907708c
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-17 20:39:54 +0900

    update: protectedRoute 로직 수정 (리코일 -> useVerifyUser)

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { userIsLoggedInSelector } from 'recoil/userExample';
+import { useVerifyUser } from 'hooks';
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
   const isLoggedIn = useRecoilValue(userIsLoggedInSelector);
-  return isLoggedIn ? element : <Navigate to="/login" replace />;
+  const { data, isLoading, isError } = useVerifyUser(!isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoading && isError) {
+      window.alert('로그인이 필요합니다.');
+    }
+  }, [isLoading, isError]);
+  return isLoggedIn || (data && data.nickname) ? (
+    element
+  ) : (
+    <Navigate to="/login" replace />
+  );
 };
 
 export default ProtectedRoute;
