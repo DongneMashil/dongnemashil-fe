7e63674e6520b79f599dffb67bab9c3af805ebce
soolovepat <soolovepat@naver.com>
2023-08-18 10:52:29 +0900

    fix : Map 현 스크롤 위치에서 뜨도록 position 수정

--- src/pages/SearchResultMapPage/SearchResultMapPage.styles.ts
+++ src/pages/SearchResultMapPage/SearchResultMapPage.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StResultMapContainer = styled.div`
   width: 100%;
   height: 100%;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   z-index: 1000;
