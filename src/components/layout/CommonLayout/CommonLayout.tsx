76650cfd1ae3d2f0f35447dd0e89c4ecf6021491
Taehyun Kim <agart319@gmail.com>
2023-08-23 17:06:48 +0900

    fix: ios 크롬 레이아웃 깨짐 수정

--- src/components/layout/CommonLayout/CommonLayout.tsx
+++ src/components/layout/CommonLayout/CommonLayout.tsx
@@ -64,7 +64,13 @@ export const CommonLayout: React.FC<CommonLayoutProps> = ({
       }
     };
   }, [prevPosition]);
+  const setVH = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
 
+  window.addEventListener('resize', setVH);
+  setVH();
   return (
     <StLayoutOuter>
       <StLayoutBody>
