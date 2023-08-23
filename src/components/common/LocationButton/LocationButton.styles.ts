1f5c134ee77750d0c6840d5372e0cb5e810a77e5
Taehyun Kim <agart319@gmail.com>
2023-08-23 16:49:09 +0900

    chore: 주석제거

--- src/components/common/LocationButton/LocationButton.styles.ts
+++ src/components/common/LocationButton/LocationButton.styles.ts
@@ -1,20 +1,13 @@
 import { styled } from 'styled-components';
 
 export const StLocationButton = styled.button`
-  //   position: absolute;
-  //   bottom: 30px;
-  //   right: 10px;
-  z-index: 1;
-  width: 3.3125rem;
-  height: 3.3125rem;
-  background-color: #fff;
-  border-radius: 100%;
-
-  //   top: 0.94rem;
-  //   left: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 1000;
+  width: 3.3125rem;
+  height: 3.3125rem;
+  z-index: 10;
+  background-color: #fff;
+  border-radius: 100%;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
 `;
