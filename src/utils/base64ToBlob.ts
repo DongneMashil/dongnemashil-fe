84c0d0756a3fe2a14037e99244ec998fae751318
Taehyun Kim <agart319@gmail.com>
2023-08-26 16:36:55 +0900

    update: base64 변환 함수 예외처리 추가

--- src/utils/base64ToBlob.ts
+++ src/utils/base64ToBlob.ts
@@ -1,11 +1,16 @@
-export function base64ToBlob(base64: string, mimeType: string): Blob {
-  const byteString = atob(base64.split(',')[1]);
-  const arrayBuffer = new ArrayBuffer(byteString.length);
-  const uint8Array = new Uint8Array(arrayBuffer);
+export function base64ToBlob(data: string, mimeType: string): Blob {
+  if (data.startsWith('data:')) {
+    const byteString = atob(data.split(',')[1]);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const uint8Array = new Uint8Array(arrayBuffer);
 
-  for (let i = 0; i < byteString.length; i++) {
-    uint8Array[i] = byteString.charCodeAt(i);
+    for (let i = 0; i < byteString.length; i++) {
+      uint8Array[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([uint8Array], { type: mimeType });
   }
 
-  return new Blob([uint8Array], { type: mimeType });
+  // base64가 아닌 경우 바로 보내줌.
+  return new Blob([data], { type: mimeType });
 }
