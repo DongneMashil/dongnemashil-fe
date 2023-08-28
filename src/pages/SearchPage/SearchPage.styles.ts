84ca27c1464d284bc6a6ffb98eabb041419a2b04
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-28 22:04:52 +0900

    design: 검색 화면 input 포커스 스타일링 추가

--- src/pages/SearchPage/SearchPage.styles.ts
+++ src/pages/SearchPage/SearchPage.styles.ts
@@ -22,6 +22,7 @@ export const StSearchInput = styled(StInput)`
   width: 166px;
   border-radius: 25px;
   margin: 12px 0;
+  padding: 0 1.2rem;
   text-align: center;
 
   &::placeholder {
@@ -31,6 +32,10 @@ export const StSearchInput = styled(StInput)`
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+  }
+
+  &:focus {
+    background-color: #ffffff;
   }
 `;
 
