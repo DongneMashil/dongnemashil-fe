2c1095052b29f8485840055938322d4042ffd384
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-28 20:42:50 +0900

    design: 로그인 선택 페이지 반응형 적용

--- src/components/commonLoginPage/LoginBtnWrapper/LoginBtnWrapper.styles.ts
+++ src/components/commonLoginPage/LoginBtnWrapper/LoginBtnWrapper.styles.ts
@@ -2,5 +2,8 @@ import styled from 'styled-components';
 
 export const StLoginButtonWrapper = styled.div`
   width: 100%;
-  padding: 0 22px;
+  padding: 0 19px;
+  @media ${(props) => props.theme.device.tablet} {
+    padding: 0 22px;
+  }
 `;
