2c207f1ad33b50ffed2b234038e2f5698dfaa156
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-29 12:35:42 +0900

    design: 로고 일반 로그인 반응형 적용

--- src/components/common/AuthLogoBox/AuthLogoBox.styles.ts
+++ src/components/common/AuthLogoBox/AuthLogoBox.styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export interface AuthLogoBoxProps {
   $align: 'left' | 'center' | 'right';
+  $page: 'login' | 'register' | 'commonLogin';
 }
 
 export const StLogoBox = styled.div<AuthLogoBoxProps>`
