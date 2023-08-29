26d34202888116e5d835a58f510326bde87b7484
Seung Hyun Tark <makepin2r@gmail.com>
2023-08-29 12:30:34 +0900

    update: 뷰포트 크기 감지 함수 추가

--- src/components/common/AuthLogoBox/AuthLogoBox.tsx
+++ src/components/common/AuthLogoBox/AuthLogoBox.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as LogoImage } from 'assets/icons/LogoHorizontal.svg';
+import { throttle } from 'lodash';
+import { ReactComponent as LogoImage } from 'assets/logo/LogoHorizontal.svg';
+import { ReactComponent as LogoImageLg } from 'assets/logo/LogoHorizontalLg.svg';
 import { StLogoBox } from './AuthLogoBox.styles';
 
 export const AuthLogoBox = React.memo(
-  ({ align = 'left' }: { align?: 'left' | 'center' | 'right' }) => {
+  ({
+    align = 'left',
+    page = 'commonLogin',
+  }: {
+    align?: 'left' | 'center' | 'right';
+    page?: 'login' | 'register' | 'commonLogin';
+  }) => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleSize = throttle(() => {
+      setWidth(window.innerWidth);
+      console.log(window.innerWidth);
+    }, 200);
+
+    useEffect(() => {
+      window.addEventListener('resize', handleSize);
+      return () => {
+        window.removeEventListener('resize', handleSize);
+      };
+    }, []);
+
     return (
-      <StLogoBox $align={align}>
-        <Link to="/">
-          <LogoImage />
-        </Link>
+      <StLogoBox $align={align} $page={page}>
+        <Link to="/">{width >= 340 ? <LogoImageLg /> : <LogoImage />}</Link>
       </StLogoBox>
     );
   }
