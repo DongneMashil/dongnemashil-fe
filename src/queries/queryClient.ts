e75713dc5b0d66a25bf0f17b5dbf484eff76b2e8
Taehyun Kim <agart319@gmail.com>
2023-08-31 01:27:32 +0900

    update: 쿼리 기본설정으로 주석

--- src/queries/queryClient.ts
+++ src/queries/queryClient.ts
@@ -3,9 +3,9 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: false,
+      // refetchOnWindowFocus: false,
+      // refetchOnReconnect: false,
+      // retry: false,
     },
   },
 });
