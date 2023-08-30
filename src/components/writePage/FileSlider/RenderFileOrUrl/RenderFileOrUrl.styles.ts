d529e238d95ae72897558fcb5516677da6a8cf20
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-08-31 03:40:23 +0900

    chore: 투명도 제거

--- src/components/writePage/FileSlider/RenderFileOrUrl/RenderFileOrUrl.styles.ts
+++ src/components/writePage/FileSlider/RenderFileOrUrl/RenderFileOrUrl.styles.ts
@@ -38,7 +38,7 @@ export const StCoverImageButton = styled.button<{ isActive: boolean }>`
   width: 2.4rem;
   height: 2.4rem;
   position: absolute;
-  top: 0.69rem;
+  top: 1.19rem;
   left: 0.69rem;
   background: ${(props) =>
     props.isActive ? '#9A7B9A' : 'rgba(154, 123, 154, 0.35)'};
@@ -49,7 +49,6 @@ export const StCoverImageButton = styled.button<{ isActive: boolean }>`
   border: ${(props) => (props.isActive ? 'none' : '1.5px solid white')};
   border-radius: 100%;
   cursor: pointer;
-  opacity: 0.8;
 
   &:hover {
     opacity: 1;
