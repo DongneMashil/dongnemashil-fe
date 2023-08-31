d2ff7b66704d2953d4aaf668d671408bf3ea09e9
Seung Hyun Tark <makepin2r@gmail.com>
2023-09-01 03:54:54 +0900

    design: 검색 화면 디자인 수정중

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthNavButton } from 'components/common';
 import { SearchHeader } from 'components/searchPage/SearchHeader/SearchHeader';
-import { StSearchInput, StSearchHeader } from './SearchPage.styles';
+import { ReactComponent as DeleteIcon } from 'assets/icons/DeleteXMark.svg';
+import { ReactComponent as InputIcon } from 'assets/icons/SearchPageIcon.svg';
+import {
+  StSearchWrapper,
+  StSearchInput,
+  StSearchContainer,
+  StSearchBox,
+  StSearchInputBox,
+  StRecentKeywordsWrapper,
+  StRecentKeywordsBox,
+} from './SearchPage.styles';
 
 export const SearchPage = () => {
   const navigate = useNavigate();
@@ -11,29 +21,48 @@ export const SearchPage = () => {
     setValue(e.target.value);
   };
   const search = () => {
-    // console.log('Searching... 검색어: ', value);
     navigate(`/search/result?q=${value}`);
   };
   const onKeyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      // console.log('onKeyPressHandler');
       search();
     }
   };
   return (
-    <StSearchHeader>
-      <AuthNavButton type="back" />
-      <SearchHeader />
-      <StSearchInput
-        type="text"
-        name="searchQuery"
-        id="searchQuery"
-        value={value}
-        onChange={onChangeHandler}
-        onKeyDown={onKeyPressHandler}
-        placeholder="예) 강남구"
-      />
-    </StSearchHeader>
+    <StSearchContainer>
+      <StSearchWrapper>
+        <StSearchBox>
+          <AuthNavButton type="back" />
+          <SearchHeader />
+          <StSearchInputBox>
+            <StSearchInput
+              type="text"
+              name="searchQuery"
+              id="searchQuery"
+              value={value}
+              onChange={onChangeHandler}
+              onKeyDown={onKeyPressHandler}
+              placeholder="예) 강남구"
+            />
+            <InputIcon />
+          </StSearchInputBox>
+        </StSearchBox>
+      </StSearchWrapper>
+      <StRecentKeywordsWrapper>
+        <p>최근 검색어</p>
+        <StRecentKeywordsBox>
+          <li>
+            한강대로 <DeleteIcon />
+          </li>
+          <li>
+            한강대로 <DeleteIcon />
+          </li>
+          <li>
+            한강대로 <DeleteIcon />
+          </li>
+        </StRecentKeywordsBox>
+      </StRecentKeywordsWrapper>
+    </StSearchContainer>
   );
 };
