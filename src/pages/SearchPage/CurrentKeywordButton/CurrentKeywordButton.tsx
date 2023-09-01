833e65e91b2ddcabcfe5e127fbbf210aea9ca203
Seung Hyun Tark <makepin2r@gmail.com>
2023-09-01 11:42:16 +0900

    update: 최근 검색어 키워드 컴포넌트 수정 (진행중)

--- src/pages/SearchPage/CurrentKeywordButton/CurrentKeywordButton.tsx
+++ src/pages/SearchPage/CurrentKeywordButton/CurrentKeywordButton.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-export const CurrentKeywordButton = ({children, onClick} : {children: string; onClick: (e:React.MouseEvent<HTMLElement>)}) => {
-    const onQuery = (e:React.MouseEvent<HTMLElement>) => {
-        onClick(e);
-    }
-  return (
-    <button onClick={onQuery}>{children}</button>
-  )
-}
+export const CurrentKeywordButton = ({
+  children,
+  onClick,
+}: {
+  children: string;
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
+}) => {
+  const onQuery = (e: React.MouseEvent<HTMLElement>) => {
+    onClick(e);
+  };
+  return <button onClick={onQuery}>{children}</button>;
+};
