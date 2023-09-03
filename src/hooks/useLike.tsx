4659560bd7348a5a2476cef50c594703d3eff7e7
Taehyun Kim <agart319@gmail.com>
2023-09-04 01:45:46 +0900

    update: 좋아요로직 정리

--- src/hooks/useLike.tsx
+++ src/hooks/useLike.tsx
@@ -55,13 +55,6 @@ export const useLike = ({
         // API 응답과 낙관적 업데이트의 결과가 다르면 api기준으로 업데이트
         setIsLiked(result); // API 응답으로 결과만 업데이트
         setLikeCnt(likeCnt); // 원래의 좋아요 수로 되돌립니다.
-        //(이 페이지 진입 후, 별도의 페이지에서 이용자가 수정을 했다고 가정하고, 기존 숫자가 맞는걸로 가정함)
-        setTimeout(
-          () => {
-            setLastClickedTime(null); // 0.5초 후 클릭 가능 상태로 변경
-          },
-          500 - (Date.now() - (lastClickedTime || Date.now()))
-        );
       }
     } catch (error) {
       console.log('좋아요 처리 중 오류가 발생했습니다.'); // 오류 처리
@@ -76,45 +69,11 @@ export const useLike = ({
       setUserState(newData);
       setIsLiked(previousIsLiked);
       setLikeCnt(likeCnt);
-
-      setTimeout(
-        () => {
-          setLastClickedTime(null); // 0.5초 후 클릭 가능 상태로 변경
-        },
-        500 - (Date.now() - (lastClickedTime || Date.now()))
-      );
     } finally {
       clearTimeout(forceEndLoadingTimeout); // 강제로 종료하는 timeout을 제거합니다.
-
       setLoading(false);
     }
   };
 
   return { isLiked, likeCnt, toggleLikeHandler };
 };
-
-/**
- * 사용 예시
- * 
-import { ReactComponent as Heart } from 'assets/icons/Heart.svg';
-import { ReactComponent as FilledHeart } from 'assets/icons/HeartFilled.svg';
-import { useLike } from 'hooks/useLike';
-//중략//
-
-const { isLiked, likeCnt, toggleLikeHandler, canClick } = useLike({  // canClick 추가
-  reviewId: [해당 reviewId string으로 여기 넣기],
-  initialIsLiked: [서버에서 받은 isliked 여기에 넣기],
-  initialLikeCnt: [서버에서 받은 likeCnt 여기에 넣기],
-});
-
-
-  return (
-    <div>
-      <button onClick={() => canClick && toggleLike()}>
-     {isLiked ? <FilledHeart /> : <Heart />}
-      </button>
-      <p>{likeCnt}좋아요 숫자 표시</p>
-    </div>
-  );
-}
- */
