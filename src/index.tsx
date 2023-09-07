e22e61da3fa3869aa841e8d3950aa0a677175165
soolovepat <soolovepat@naver.com>
2023-09-07 21:48:13 +0900

    delete : msw 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { worker } from 'mocks/browser';
-
-// if (process.env.NODE_ENV === 'development') {
-//   worker.start();
-// }
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
