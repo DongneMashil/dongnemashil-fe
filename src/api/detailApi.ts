20d171885266604045fc10c0ae7dd01c4acf3528
Taehyun Kim <agart319@gmail.com>
2023-09-08 17:49:31 +0900

    refactor: 변수명 변경

--- src/api/detailApi.ts
+++ src/api/detailApi.ts
@@ -144,7 +144,7 @@ export const postLikeOptimistic = async (
   currentState: boolean // 현재 좋아요 상태를 받아옵니다.
 ): Promise<boolean> => {
   //  UI를 즉시 업데이트합니다. (낙관적 업데이트)
-  const updatedState = !currentState;
+  const expectedState = !currentState;
 
   try {
     // 백엔드에 비동기 요청을 보냅니다.
@@ -153,13 +153,14 @@ export const postLikeOptimistic = async (
 
     if (
       //예상과 반대의 값이 올때
-      (updatedState === true && response.data.message === '좋아요 취소 완료') ||
-      (updatedState === false && response.data.message === '좋아요 완료')
+      (expectedState === true &&
+        response.data.message === '좋아요 취소 완료') ||
+      (expectedState === false && response.data.message === '좋아요 완료')
     ) {
       return currentState;
     }
 
-    return updatedState;
+    return expectedState;
   } catch (e: unknown) {
     // 서버와 통신이 어려울 경우 UI를 이전 상태로 롤백하고 사용자에게 오류 메시지를 표시합니다.
     if (e instanceof AxiosError) {
